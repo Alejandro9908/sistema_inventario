42260a4d879dde3755095eabdc35c22d5005d951
Alejandro9908 <joc.alejandro99@gmail.com>
2020-05-24 11:49:11 -0600

    se completo todos los cambios

--- resources/views/producto/categoria/edit.blade.php
+++ resources/views/producto/categoria/edit.blade.php
@@ -2,8 +2,11 @@
 @section ('contenido')
 	<div class="row">
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-            <h3>Editar Categoría:</h3>
-            <hr>
+			<h3>Categoría:</h3>
+			<button class="btn btn-info" id="btnEditar"><i class="fa fa-pencil"></i>   Editar</button>
+			<hr>
+			
+			<br>
 			@if (count($errors)>0)
 			<div class="alert alert-danger">
 				<ul>
@@ -18,20 +21,43 @@
             {{Form::token()}}
             <div class="form-group">
             	<label for="nombre">Nombre</label>
-            	<input type="text" name="nombre_categoria" class="form-control" value="{{$categoria->nombre_categoria}}" placeholder="Nombre...">
+            	<input disabled type="text" name="nombre_categoria" id="nombre_categoria" class="form-control" value="{{$categoria->nombre_categoria}}" placeholder="Nombre...">
             </div>
             <div class="form-group">
             	<label for="descripcion">Descripción</label>
-            	<input type="text" name="descripcion" class="form-control" value="{{$categoria->descripcion}}" placeholder="Descripción...">
+            	<input disabled type="text" name="descripcion" id="descripcion" class="form-control" value="{{$categoria->descripcion}}" placeholder="Descripción...">
             </div>
-            <hr>
-            <div class="form-group">
-                <button class="btn btn-primary" type="submit">Guardar Cambios</button>
-            	<button class="btn btn-default" type="reset">Cancelar</button>
+			<hr>
+			
+            <div class="form-group" id="botones" style="display: none">
+				
+                <button class="btn btn-primary" type="submit"><i class="fa fa-check-square-o"></i> Guardar Cambios</button>
+				
+				<button class="btn btn-danger" type="reset" id="btnCancelar"><i class="fa fa-remove"></i> Cancelar</button>
+				
             </div>
                  
 			{!!Form::close()!!}		
             
 		</div>
 	</div>
+
+	@push('scripts')
+	<script>
+		$(document).ready(function(){
+			$("#btnEditar").click(function(){
+				$("#descripcion").prop('disabled', false);
+				$("#nombre_categoria").prop('disabled', false);
+				$("#botones").show();
+			});
+
+			$("#btnCancelar").click(function(){
+				$("#descripcion").prop('disabled', true);
+				$("#nombre_categoria").prop('disabled', true);
+				$("#botones").hide();
+			});
+		});
+
+	</script>
+@endpush
 @endsection
